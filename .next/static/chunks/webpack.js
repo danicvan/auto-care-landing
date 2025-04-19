0cb0ec15bd42ab6306237bfc5a59eb9f36960b07
daniel <danicvan@hotmail.com>
2025-04-19 18:11:02 -0300

    feat: create redirect checkout page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = (chunkId) => {
 /******/ 			// return url for filenames based on template
-/******/ 			return undefined;
+/******/ 			return "static/chunks/" + chunkId + ".js";
 /******/ 		};
 /******/ 	})();
 /******/ 	
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("6226adc752c05ec8")
+/******/ 		__webpack_require__.h = () => ("2c3be159852336f2")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
